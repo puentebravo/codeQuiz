c951692a4e552969940c8507c67601a3b47ee4c0
Puentebravo <puentematos@gmail.com>
2020-11-30 23:24:42 -0500

    wrote code for the game's timer function

--- logic.js
+++ logic.js
@@ -1,4 +1,10 @@
 console.log("IS WORKING");
+var timerEl = document.querySelector("#timeRemaining");
+var questionEl = document.querySelector("#qBlock");
+var choiceEl = document.querySelector("#chBlock");
+var statusEl = document.querySelector("#answerStat");
+var fameList = document.querySelector("#fameList");
+var startBtn = document.querySelector("#startBtn");
 
 // Array of objects to load quiz questions from
 
@@ -6,6 +12,7 @@ var question1 = {
   question: "What kind of code makes up the backbone of a website?",
   choices: ["The Matrix", "CSS", "HTML", "Javascript"],
   answer: "HTML",
+  value: 5,
 };
 
 console.log(question1);
@@ -14,6 +21,7 @@ var question2 = {
   question: "In CSS, how do you refer to a specific ID?",
   choices: ["#", "by its html element", ".", "{}"],
   answer: "#",
+  value: 5,
 };
 
 console.log(question2);
@@ -27,12 +35,14 @@ var question3 = {
     "execute a function a certain number of times",
   ],
   answer: "execute a function until a condition is met",
+  value: 5,
 };
 
 var question4 = {
   question: "An array must be enclosed in which characters?",
   choices: [" '  '", "< >", "( )", "[ ]"],
   answer: "[ ]",
+  value: 5,
 };
 
 var question5 = {
@@ -40,6 +50,7 @@ var question5 = {
     "Last one. What language uses $ and a CDN to replace a significant amount of Javascript syntax?",
   choices: ["JSON", "Node.js", "JQuery", "SQL"],
   answer: "Jquery",
+  value: 5,
 };
 
 var bonus = {
@@ -52,6 +63,7 @@ var bonus = {
     "(Random being flung into the pit noises)",
   ],
   answer: "Roundabouts 24 miles per hour, if it's European.",
+  value: 2,
 };
 
 var qArray = [question1, question2, question3, question4, question5, bonus];
@@ -59,20 +71,26 @@ console.log(qArray);
 
 var time = 30;
 var score = 0;
-
-var timerEl = document.querySelector("#timeRemaining");
-var questionEl = document.querySelector("#qBlock");
-var choiceEl = document.querySelector("#chBlock");
-var statusEl = document.querySelector("#answerStat");
-var fameList = document.querySelector("#fameList");
-
-//var timeElapsed = 0;
-//var timeLeft = time - timeElapsed;
+var timeElapsed = 0;
+var timeLeft = time - timeElapsed;
+var interval;
 
 //Timer function to count down- look back at the tomato timer project, see how that worked.
 
-//While loop to keep cycling through the array while there's time left
+function timeDisplay() {
+  timerEl.textContent = "Time Remaining: " + timeLeft + " seconds.";
+}
 
+function gameTimer() {
+  if (timeLeft > 0) {
+    interval = setInterval(function () {
+      timeElapsed++;
+      timeDisplay();
+    }, 1000);
+  }
+}
+//for loop to keep cycling through the array while there's time left
+function startGame() {}
 //Function to subtract seconds from timer for wrong answers
 
 //Array of objects: each object is a question, with properties for the question itself, its choices, and its answer key
