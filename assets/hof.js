4a7a1c3d5a0fd07d2f71e38da1ff7f2d6af4485e
Puentebravo <puentematos@gmail.com>
2020-12-04 01:45:05 -0500

    Added clear storage function for "clear hall" button, allowing user to clear local storage

--- assets/hof.js
+++ assets/hof.js
@@ -1,1 +1,21 @@
-console.log("Link is up");
+var fameList = document.querySelector("#fameList");
+var clrBtn = document.querySelector("#clrHall");
+
+function renderScore() {
+  var highScore = JSON.parse(localStorage.getItem("highScore"));
+  var hLi = document.createElement("li");
+  hLi.setAttribute("class", "list-group-item");
+  fameList.appendChild(hLi);
+  hLi.textContent = highScore;
+}
+
+function clearHall() {
+  localStorage.clear();
+  location.reload();
+}
+
+document.addEventListener("DOMContentLoaded", renderScore());
+clrBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  clearHall();
+});
